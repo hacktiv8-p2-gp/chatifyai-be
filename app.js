bf32b3fafc3c4a098635d0f5175d2fe3f154de06
Leonardo-1998 <leonardo.agst98@gmail.com>
2025-05-08 10:34:16 +0700

    feat : Penggantian env untuk production

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const io = new Server(server, {
     origin: "*",
   },
 });
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 const FriendRouter = require("./routers/FriendRouter");
 const ConversationRouter = require("./routers/ConversationRouter");
